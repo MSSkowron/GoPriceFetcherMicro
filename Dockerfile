23b087114799e8a86894c423246665077444ac37
MSSkowron <mateusz.skowron@poczta.onet.pl>
2023-01-07 12:32:59 +0100

    Add docker-compose

--- Dockerfile
+++ Dockerfile
@@ -10,5 +10,6 @@ COPY . ./
 RUN go build -o /pricefetcher
 
 EXPOSE 3000 
+EXPOSE 4000
 
 CMD ["/pricefetcher"]
--- main.go
+++ main.go
@@ -1,14 +1,8 @@
 package main
 
 import (
-	"context"
 	"flag"
-	"fmt"
 	"log"
-	"time"
-
-	"github.com/MSSkowron/GoMicroPriceFetcher/client"
-	"github.com/MSSkowron/GoMicroPriceFetcher/proto"
 )
 
 func main() {
@@ -16,25 +10,8 @@ func main() {
 		jsonAddr = flag.String("json", ":3000", "listen address the service is running")
 		grpcAddr = flag.String("grpc", ":4000", "listen address the service is running")
 		svc      = NewLoggingService(NewMetricsService(&priceFetcher{}))
-		ctx      = context.Background()
 	)
 	flag.Parse()
-
-	grpcClient, err := client.NewGRPCClient(":4000")
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	go func() {
-		time.Sleep(3 * time.Second)
-
-		resp, err := grpcClient.FetchPrice(ctx, &proto.PriceRequest{Ticker: "BTC"})
-		if err != nil {
-			log.Fatalln(err)
-		}
-
-		fmt.Printf("%+v\n", resp)
-	}()
 
 	go func() {
 		if err := makeGRPCServerAndRun(*grpcAddr, svc); err != nil {
